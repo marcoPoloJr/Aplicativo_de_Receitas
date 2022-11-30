a61a7731034d349f8a1b99e5181951801fef44a6
Marco Polo <seguros.marcopolo@gmail.com>
2022-11-30 17:25:39 -0300

    requisIto 57 finalizado

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div className="meals">
+      <Profile />
       <span className="logo">TRYBE</span>
       <object
         className="rocksGlass"
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Profile() {
+    
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p data-testid="profile-email">email@email.com</p>
+      <button 
+      type='button'
+      data-testid="profile-favorite-btn"
+      onClick={()=> console.log('Done Recipes')}>
+        Done Recipes
+       </button>
+       <button 
+      type='button'
+      data-testid="profile-favorite-btn"
+      onClick={()=> console.log("Favorite Recipes")}>
+        Favorite Recipes
+        </button>
+        <button 
+      type='button'
+      data-testid="profile-logout-btn"
+      onClick={()=> console.log("Logout")}>
+        Logout
+        </button>
+    </div>
+
+  );
+}
+
+export default Profile;
