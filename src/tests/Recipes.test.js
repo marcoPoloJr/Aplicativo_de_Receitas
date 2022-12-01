c160bf83f6d018cdb4b5ad4ecb5b85b7a23814bc
othiagomanhaes <othiagomanhaes@gmail.com>
2022-12-01 02:07:35 -0300

    Requisito 20.

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,25 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import Recipe from '../components/recipe/Recipe';
+import { screen } from '@testing-library/react';
+import Recipes from '../components/recipe/Recipes';
 import RecipesProvider from '../context/RecipesProvider';
 // import App from '../App';
 import { renderWithRouter } from './renders/RenderWithRouter';
 
 describe('Testes do Componente Recipe', () => {
+  // beforeEach(() => {
+  //   jest.spyOn(global, 'fetch');
+  //   global.fetch.mockResolvedValue({
+  //     json: jest.fn().mockResolvedValue(meals),
+  //   });
+  // });
+  // afterEach(() => {
+  //   global.fetch.mockClear();
+  // });
   test('se o botão Beef está na tela', async () => {
     const { history } = renderWithRouter(
       <RecipesProvider>
-        <Recipe />
+        <Recipes />
       </RecipesProvider>,
     );
     history.push('/meals');
     const comidas = await screen.findByText('Comidas');
     expect(comidas).toBeInTheDocument();
-    
-    function timeout(delay: number) {
-      return new Promise( res => setTimeout(res, delay) );
-    } await timeout(1000);
-    
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+      // return 1;
+    });
+
     const btn = await screen.findByTestId('Beef-category-filter');
+
     // const vasco = screen.getByText('vasco');
     expect(btn).toBeInTheDocument();
   });
@@ -27,17 +38,18 @@ describe('Testes do Componente Recipe', () => {
   test('se a imagem da Corba está na tela', async () => {
     const { history } = renderWithRouter(
       <RecipesProvider>
-        <Recipe />
+        <Recipes />
       </RecipesProvider>,
     );
     history.push('/meals');
     const comidas = await screen.findByText('Comidas');
     expect(comidas).toBeInTheDocument();
-    
-    function timeout(delay: number) {
-      return new Promise( res => setTimeout(res, delay) );
-    } await timeout(1000);
-    
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+      // return 1;
+    });
+
     const imagemCorba = await screen.findByTestId('0-card-img');
     // const vasco = screen.getByText('vasco');
     expect(imagemCorba).toBeInTheDocument();
@@ -47,17 +59,18 @@ describe('Testes do Componente Recipe', () => {
   test('se o botão Ordinary Drinks está na tela', async () => {
     const { history } = renderWithRouter(
       <RecipesProvider>
-        <Recipe />
+        <Recipes />
       </RecipesProvider>,
     );
     history.push('/drinks');
     const bebidas = await screen.findByText('Bebidas');
     expect(bebidas).toBeInTheDocument();
-    
-    function timeout(delay: number) {
-      return new Promise( res => setTimeout(res, delay) );
-    } await timeout(1000);
-    
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+      // return 1;
+    });
+
     const btn = await screen.findByTestId('Ordinary Drink-category-filter');
     // const vasco = screen.getByText('vasco');
     expect(btn).toBeInTheDocument();
