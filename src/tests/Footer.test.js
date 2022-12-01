f2eaf679345e1d634108028c66250f6e475fa5a6
Robson Miranda <robsm36@gmail.com>
2022-12-01 16:18:55 -0300

    Requisito 16 e 18

---
+++ src/tests/Footer.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Footer from '../components/Footer';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente Footer', () => {
+  test('Testa se o componente está sendo renderizado', () => {
+    render(<Footer />);
+    const footer = screen.getByTestId('footer');
+    expect(footer).toBeInTheDocument();
+  });
+  test('Testa se os elementos do footer são exibidos corretamente', () => {
+    render(<Footer />);
+    const drinkBtn = screen.getByTestId('drinks-bottom-btn');
+    const mealBtn = screen.getByTestId('meals-bottom-btn');
+    expect(drinkBtn).toBeInTheDocument();
+    expect(mealBtn).toBeInTheDocument();
+  });
+  test('Testa se os redirecionamentos de rota estão ocorrendo corretamente', () => {
+    const { history } = renderWithRouter(<Footer />);
+    const drinkBtn = screen.getByTestId('drinks-bottom-btn');
+    userEvent.click(drinkBtn);
+    expect(history.location.pathname).toBe('/drinks');
+  });
+});
