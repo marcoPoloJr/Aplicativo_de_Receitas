167ad6ed7bb35b20b7c6f517be6b7144b40ab88b
Robson Miranda <robsm36@gmail.com>
2022-12-01 16:51:40 -0300

    Correção lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import RecipesProvider from './context/RecipesProvider';
