1ba04c634576f8ce8d611932d274d6ce0359b859
Marco Polo <seguros.marcopolo@gmail.com>
2022-12-01 13:43:20 -0300

    requisIto 57 correção lint e rotas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Recipes from './components/recipe/Recipes';
 import Profile from './components/Profile/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <Profile />
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        {/* <Route exact patch="/" component={ () => {} } /> */}
+        <Route exact patch="/meals" component={ () => {} } />
+        <Route exact patch="/drinks" component={ () => {} } />
+        <Route exact patch="/meals/:id-da-receita" component={ () => {} } />
+        <Route exact patch="/drinks/:id-da-receita" component={ () => {} } />
+        <Route patch="/meals/:id-da-receita/in-progress" component={ () => {} } />
+        <Route patch="/drinks/:id-da-receita/in-progress" component={ () => {} } />
+        <Route patch="/profile" component={ Profile} />
+        <Route patch="/done-recipes" component={ () => {} } />
+        <Route patch="/favorite-recipes" component={ () => {} } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
