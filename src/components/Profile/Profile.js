bb5f12532d000246351843f6dcb88c1172da024f
Marco Polo <seguros.marcopolo@gmail.com>
2022-12-01 13:33:23 -0300

    requisIto 57 correção lint

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,29 +1,31 @@
 import React from 'react';
 
 function Profile() {
-    
   return (
     <div>
       <h1>Profile</h1>
       <p data-testid="profile-email">email@email.com</p>
-      <button 
-      type='button'
-      data-testid="profile-favorite-btn"
-      onClick={()=> console.log('Done Recipes')}>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => console.log('Done Recipes') }
+      >
         Done Recipes
-       </button>
-       <button 
-      type='button'
-      data-testid="profile-favorite-btn"
-      onClick={()=> console.log("Favorite Recipes")}>
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => console.log('Favorite Recipes') }
+      >
         Favorite Recipes
-        </button>
-        <button 
-      type='button'
-      data-testid="profile-logout-btn"
-      onClick={()=> console.log("Logout")}>
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => console.log('Logout') }
+      >
         Logout
-        </button>
+      </button>
     </div>
 
   );
