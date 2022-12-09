03027f11726ea9913df92f80f669b305166c6a8f
Robson Miranda <robsm36@gmail.com>
2022-12-09 01:24:19 -0300

    Correções

--- src/components/FavoriteRecipes.js
+++ src/components/FavoriteRecipes.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import blackHeart from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
+import Header from './Header/Header';
 
 function FavoriteRecipes() {
   const [allFavoriteRecipes, setAllFavoriteRecipes] = useState([]);
@@ -30,6 +31,7 @@ function FavoriteRecipes() {
 
   return (
     <div>
+      <Header />
       { allFavoriteRecipes
         ? (
           <div>
