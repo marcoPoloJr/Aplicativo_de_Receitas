708f96498a718b7bd4b72b98fa768eacfe76c5cd
Robson Miranda <robsm36@gmail.com>
2022-12-09 01:05:21 -0300

    Requisitos 4 ao 6 E testes de outros requisitos

---
+++ src/tests/RecipeDetails.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import { renderWithRouter } from './renders/RenderWithRouter';
+
+describe('Testa o componente RecipeDetails', () => {
+  test('Testa se o componente está sendo renderizado corretamente', async () => {
+    const { history } = renderWithRouter(
+      <App />,
+    );
+
+    act(() => {
+      history.push('/meals/52977');
+    });
+
+    expect(history.location.pathname).toEqual('/meals/52977');
+    expect(global.fetch).toHaveBeenCalled();
+
+    // const recipePhoto = await screen.findByTestId('recipe-photo');
+    // expect(recipePhoto).toBeInTheDocument();
+  });
+});
